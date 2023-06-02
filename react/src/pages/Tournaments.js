e11cb6d174e3391ab18def2f85d5de7f3b5dc96f
Arnaud <arnaudcrespel27@gmail.com>
2023-06-02 13:53:18 +0200

    update

--- react/src/pages/Tournaments.js
+++ react/src/pages/Tournaments.js
@@ -3,36 +3,8 @@ import React from "react";
 const Tournaments = () => {
   return (
     <div>
-      <a href="https://mega.nz/folder/ewdSjBAA#DS_T7i0y00Z5-VsOsgbPug" target="_blank" rel="noopener noreferrer">Download Excel file Here</a>
-      <h1>Excel file with all Tennis matches available :</h1>
-      <br />
-      <p>
-        Australian Open
-        <div>
-      <a href="https://docs.google.com/spreadsheets/d/121n39spSYxX9LnkazFFRkgNIV5FsWsYE/edit?usp=sharing&ouid=106127342872488144596&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Download Excel file Here</a>
-    </div>
-      </p>
-      <br />
-      <p>
-        French Open
-        <div>
-      <a href="https://docs.google.com/spreadsheets/d/121n39spSYxX9LnkazFFRkgNIV5FsWsYE/edit?usp=sharing&ouid=106127342872488144596&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Download Excel file Here</a>
-    </div>
-      </p>
-      <br />
-      <p>
-        Wimbledon
-        <div>
-      <a href="https://docs.google.com/spreadsheets/d/121n39spSYxX9LnkazFFRkgNIV5FsWsYE/edit?usp=sharing&ouid=106127342872488144596&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Download Excel file Here</a>
-    </div>
-      </p>
-      <br />
-      <p>
-        US Open
-        <div>
-      <a href="https://docs.google.com/spreadsheets/d/121n39spSYxX9LnkazFFRkgNIV5FsWsYE/edit?usp=sharing&ouid=106127342872488144596&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Download Excel file Here</a>
-    </div>
-      </p>
+      <iframe src="https://onedrive.live.com/embed?cid=D565A765DFD3CF9C&resid=D565A765DFD3CF9C%2116390&authkey=AM08GnkDIrahq-s&em=2" width="1900" height="500" frameborder="0" scrolling="no"></iframe>
+      
     </div>
   );
 };
