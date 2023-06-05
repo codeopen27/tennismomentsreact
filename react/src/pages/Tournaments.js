b6886b8a93e62cc4130880aad720d05233d695a6
Arnaud <arnaudcrespel27@gmail.com>
2023-06-05 09:32:05 +0200

    update

--- react/src/pages/Tournaments.js
+++ react/src/pages/Tournaments.js
@@ -3,6 +3,7 @@ import React from "react";
 const Tournaments = () => {
   return (
     <div>
+      <center><h2>Please copy and paste the Tennis Matches of you choice into the Contact page and it will be upload, once ready you will get an email with a link for download it.</h2></center>
       <iframe src="https://onedrive.live.com/embed?cid=D565A765DFD3CF9C&resid=D565A765DFD3CF9C%2116390&authkey=AM08GnkDIrahq-s&em=2" width="1900" height="500" frameborder="0" scrolling="no"></iframe>
       
     </div>
