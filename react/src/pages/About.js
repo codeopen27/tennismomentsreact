375e35e952b836bd5c4e69201fc273a43326e8d3
Arnaud <arnaudcrespel27@gmail.com>
2023-06-05 09:26:41 +0200

    update

--- react/src/pages/About.js
+++ react/src/pages/About.js
@@ -7,26 +7,21 @@ const About = () => {
       <h1>A propos</h1>
       <br />
       <p>
-      Trade tennis matches, if you have some on VHS tapes or DVDs, or any questions.
-
-I am a tennis fans and collector of tennis matches, I looking for matches from 80, 90... here my tennis collection, if you have matches not list here and want to trade send me a email.
-
-Every tennis matches on this website are from different countries and the commentary are in different languages, most of them are in English, but could be in German, French, Italian, Spanish, Russian...feel free to ask me.
-Matches after 2000 are available with digital quality.
-All matches before 2000 have been transformed from VHS to DVD,
-1/2 is semi-Final, 1/4 is quarter final for info.
-Most of them are now in High definition,
-Blu-ray matches are available MORE! on demand feel free to ask tennisdvdpassion@gmail.com
-You can also download them.
-Watch for FREE livestream in LIVE or Replay all Challenger Tour ATP Click Here.
-Download tennis matches, once upload is done a link(s) is send to your email for download.
-Matches download are with the "extension .mkv" great for HD and compatibility with DVD players.
-DVD are available in all formats "Region Free" DVD will play in all regions.
-USA/Japan (NTSC) Europe/Asia/Australia (PAL)
-If any problem on your computer use VLC, the software will read all formats and zones.
-4K UltraHD ,tennis matche(s) will be on a USB Stick and can be play on your TV. USB Stick 3.0, Verbatim.
-DVD use are Verbatim DVD - R, and can be play on any players worldwide.
-Thank you for contacting me, I'll return your email as quickly as possible.
+      <h3>&#x2022; Trade tennis matches, if you have some on VHS tapes or DVDs, or any questions.</h3>
+      <h3>&#x2022; I am a tennis fans and collector of tennis matches, I looking for matches from 80, 90... here my tennis collection, if you have matches not list here and want to trade send me a email.</h3>
+      <h3>&#x2022; Every tennis matches on this website are from different countries and the commentary are in different languages, most of them are in English, but could be in German, French, Italian, Spanish, Russian...feel free to ask me.</h3>
+      <h3>&#x2022; Matches after 2000 are available with digital quality.</h3>
+      <h3>&#x2022; All matches before 2000 have been transformed from VHS to DVD,</h3>
+      <h3>&#x2022; 1/2 is semi-Final, 1/4 is quarter final for info.</h3>
+      <h3>&#x2022; Most of them are now in High definition,</h3>
+      <h3>&#x2022; Blu-ray matches are available MORE! on demand feel free to ask tennismoments27@gmail.com</h3>
+      <h3>&#x2022; You can also download them.</h3>
+      <h3>&#x2022; Watch for FREE livestream in LIVE or Replay all Challenger Tour ATP Click Here.</h3>
+      <h3>&#x2022; Download tennis matches, once upload is done a link(s) is send to your email for download.</h3>
+      <h3>&#x2022; Matches download are with the "extension .mkv" great for HD and compatibility with DVD players.</h3>
+      <h3>&#x2022; Videos are available in all formats "Region Free" DVD will play in all regions.USA/Japan (NTSC) Europe/Asia/Australia (PAL)</h3>
+      <h3>&#x2022; If any problem on your computer use VLC, the software will read all formats and zones.</h3>
+      <h3>&#x2022; Thank you for contacting me, I'll return your email as quickly as possible.</h3>
       </p>
       <br />
       <p>
